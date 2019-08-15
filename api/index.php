0c58d01908b950bac9cd21c8ca52acf3caf976f2
Akshay Sutar <akshay.sutar@plus91.in>
2019-08-15 19:33:15 +0530

    added index.php

--- api/index.php
+++ api/index.php
@@ -3,7 +3,7 @@
 	require_once __DIR__.'/../../vendor/autoload.php';
 	$oAPI = new Slim\App();
 
-	$app->get('/', function ($request, $response, $args) {
+	$oAPI->get('/', function ($request, $response, $args) {
 		$newResponse = $response->withHeader('Content-type', 'application/json')->withHeader('Access-Control-Allow-Origin', '*');
 		$newResponse = $newResponse->withJSON("Welcome!");
 		return $newResponse;	
